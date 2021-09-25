adf359294c0cfed2649a3fd3bfae8f183d02f125
Misha Sebalo <obliviusm@gmail.com>
2021-09-25 14:14:49 +0300

    Add error handling

--- app/controllers/api/v1/baskets_controller.rb
+++ app/controllers/api/v1/baskets_controller.rb
@@ -3,5 +3,10 @@ class Api::V1::BasketsController < ApplicationController
     @basket = {}
     @basket[:product_codes] = params[:product_codes]
     @basket[:price] = CalculateBasketPrice.new(@basket[:product_codes]).call
+
+    unless @basket[:price]
+      render json: { error: "At least one product code you provided is not valid" },
+        status: :unprocessable_entity
+    end
   end
 end
--- app/services/calculate_basket_price.rb
+++ app/services/calculate_basket_price.rb
@@ -4,6 +4,8 @@ class CalculateBasketPrice
   end
 
   def call
+    return if unknown_product_code?
+
     product_groups = product_codes.tally.transform_keys { |code| Product.find_by(code: code) }
     product_groups.reduce(0) do |price, product_group|
       price + GetProductsPrice.new(product: product_group[0], count: product_group[1]).call
@@ -13,4 +15,11 @@ class CalculateBasketPrice
   private
 
   attr_reader :product_codes
+
+  def unknown_product_code?
+    product_codes.uniq.each do |product_code|
+      return true unless Product.exists?(code: product_code)
+    end
+    false
+  end
 end
